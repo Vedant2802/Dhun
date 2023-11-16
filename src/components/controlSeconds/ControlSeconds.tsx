82d5c594cd0940578c62d208b35c154f390e7bfc
zohaib <zohaib.alam@zee.com>
2023-11-16 16:59:58 +0530

    timeline added

--- src/components/controlSeconds/ControlSeconds.tsx
+++ src/components/controlSeconds/ControlSeconds.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
-import TimelinePlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/timeline.esm.js";
+import { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
 import styles from "./ControlSeconds.module.scss";
+
+import RegionsPlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/regions.esm.js";
+import TimelinePlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/timeline.esm.js";
 
 interface WaveformProps {
   trackId: string;
 }
-
-const ControlSeconds: React.FC<WaveformProps> = ({ trackId }) => {
+const ControlSeconds = () => {
+  const [loop, setLoop] = useState<boolean>(false);
+  const [activeRegion, setActiveRegion] = useState<WaveSurfer.Region | null>(
+    null
+  );
   const track = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
 
   useEffect(() => {
@@ -16,34 +21,102 @@ const ControlSeconds: React.FC<WaveformProps> = ({ trackId }) => {
       return;
     }
 
-    const wsRegions = waveform.registerPlugin(RegionsPlugin.create());
-
-    //  Create an instance of WaveSurfer
-    const ws = WaveSurfer.create({
+    const waveformParams = {
       container: "#waveform",
-      waveColor: "rgb(255, 255, 255)",
-      progressColor: "rgb(255, 255, 255)",
+      waveColor: "rgb(200, 0, 200)",
+      progressColor: "rgb(100, 0, 100)",
       url: "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3",
-      minPxPerSec: 50,
-      // audioRate: 1,
-      // barWidth: 3,
-      height: 150,
+    };
+
+    const waveform = WaveSurfer.create(waveformParams);
+    const wsRegions = waveform.registerPlugin(RegionsPlugin.create());
+    waveform.registerPlugin(TimelinePlugin.create());
+    // Give regions a random color when they are created
+    const random = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+    const randomColor = () =>
+      `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`;
+
+    waveform.on("ready", () => {
+      wsRegions.addRegion({
+        start: 0,
+        end: 5,
+        content: "Track 1",
+        color: "#5b5b5b",
+        drag: false,
+        resize: true,
+      });
+      wsRegions.addRegion({
+        start: 6,
+        end: 10,
+        content: "Track 2",
+        color: "#5b5b5b",
+        minLength: 1,
+        maxLength: 10,
+      });
+      wsRegions.addRegion({
+        start: 11,
+        end: 16,
+        content: "Track 3",
+        color: "#5b5b5b",
+        resize: true,
+      });
+      wsRegions.addRegion({
+        start: 17,
+        end: 22,
+        content: "Track 4",
+        color: "#5b5b5b",
+      });
+      wsRegions.addRegion({
+        start: 23,
+        end: 30,
+        content: "Track 5",
+        color: "#5b5b5b",
+      });
+    });
+    wsRegions.enableDragSelection({
+      color: "rgba(255, 0, 0, 0.1)",
     });
 
-    const wsRegions = ws.registerPlugin(RegionsPlugin.create());
-    // Initialize the Timeline plugin
-    ws.registerPlugin(TimelinePlugin.create());
-
-    // Play on click
-    ws.on("interaction", () => {
-      ws.play();
+    wsRegions.on("region-updated", (region: any) => {
+      console.log("Updated region", region);
     });
 
-    // To start from begining
-    ws.on("finish", () => {
-      ws.setTime(0);
+    let activeRegion: null = null;
+    wsRegions.on("region-in", (region: any) => {
+      activeRegion = region;
     });
-    return () => ws.destroy();
+    wsRegions.on("region-out", (region: any) => {
+      if (activeRegion === region) {
+        if (loop) {
+          region.play();
+        } else {
+          activeRegion = null;
+        }
+      }
+    });
+
+    wsRegions.on(
+      "region-clicked",
+      (
+        region: {
+          play: () => void;
+          setOptions: (arg0: { color: string }) => void;
+        },
+        e: { stopPropagation: () => void }
+      ) => {
+        e.stopPropagation(); // prevent triggering a click on the waveform
+        const activeRegion = region;
+        region.play();
+        region.setOptions({ color: randomColor() });
+      }
+    );
+    // Reset the active region when the user clicks anywhere in the waveform
+    waveform.on("interaction", () => {
+      const activeRegion = null;
+    });
+    // Cleanup function to destroy the waveform when the component unmounts
+    return () => waveform.destroy();
   }, [trackId]);
 
   return (
--- src/components/controlSelection/ControlSelection.tsx
+++ src/components/controlSelection/ControlSelection.tsx
@@ -1,26 +1,40 @@
 import { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
 import styles from "./ConntrolSelection.module.scss";
+import addIcon from "../../../public/icons/addIcon.svg";
 
 import RegionsPlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/regions.esm.js";
 import TimelinePlugin from "https://unpkg.com/wavesurfer.js@7/dist/plugins/timeline.esm.js";
-
 interface WaveformProps {
   trackId: string;
 }
 const ControlSelection: React.FC<WaveformProps> = ({ trackId }) => {
-  // const waveformRef = useRef<HTMLDivElement>(null);
+  const [updatedRegion, setUpdatedRegion] = useState("5");
+  // Timeline to create on top
+  const topTimeline = TimelinePlugin.create({
+    insertPosition: "beforebegin",
+    height: 20,
+    timeInterval: 1,
+    primaryLabelInterval: 5,
+    secondaryLabelInterval: 0,
+    style: {
+      fontSize: "10px",
+      color: "#FFF",
+    },
+  });
 
+  // creating a waveform on given url
   const waveformParams = {
     container: "#waveform",
-    waveColor: "rgb(200, 0, 200)",
+    // waveColor: "rgb(200, 0, 200)",
     progressColor: "rgb(100, 0, 100)",
-    url: "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3",
+    height: 70,
+    url: trackId,
+    plugins: [topTimeline],
   };
+
   useEffect(() => {
     const waveform = WaveSurfer.create(waveformParams);
-    // const wsRegions = waveform.registerPlugin(RegionsPlugin.create());
-    waveform.registerPlugin(TimelinePlugin.create());
     return () => waveform.destroy();
   }, [trackId]);
 
@@ -29,18 +43,25 @@ const ControlSelection: React.FC<WaveformProps> = ({ trackId }) => {
     const wsRegions = waveform.registerPlugin(RegionsPlugin.create());
     waveform.on("ready", () => {
       wsRegions.addRegion({
-        start: 5,
-        end: 10,
+        start: 0,
+        end: 5,
         color: "rgba(255, 0, 0, 0.3)",
       });
+    });
+    wsRegions.on("region-updated", (region: any) => {
+      console.log("#Updated region", region);
+      console.log("#Updated region", region.start);
+      console.log("#Updated region", region.end);
+      console.log("#Updated region", region.totalDuration);
     });
   };
 
   return (
-    <div>
+    <div className={styles.controlContainer}>
       <div id="waveform" className={styles.waveformContainer} />
-      {/* <div ref={waveformRef} /> */}
-      <button onClick={addRegion}>Add Region</button>
+      <div onClick={addRegion} className={styles.addSongsBox}>
+        <img src={addIcon} alt="addSongs" />
+      </div>
     </div>
   );
 };
