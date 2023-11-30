0ded301391d1c7c38b8e09fa74020b8cb7d7513b
Chetan Haricharan <vc.chetan.haricha@zee.com>
2023-11-30 18:24:16 +0530

    icons added

--- src/components/resultDashboard/ResultDashboard.tsx
+++ src/components/resultDashboard/ResultDashboard.tsx
@@ -5,6 +5,8 @@ import { STUDIO_CONSTANTS } from "../../utils/genAiConstant";
 import ControlSelection from "../controlSelection/ControlSelection";
 import React, { useState } from "react";
 import ControlPanel from "../ControlsPanel/ControlPanel";
+import kebab from "../../../public/icons/KebabMenu-Vertical.svg";
+import playcircle from "../../../public/icons/play_circle_filled.svg";
 import { useGenerateStore } from "../../stores/generateStore";
 
 const ResultDashboard = () => {
@@ -56,36 +58,24 @@ const ResultDashboard = () => {
           <div className={styles.compositionContainer}>
                 <div className={styles.composition}>
                     <div className={styles.playButtonContainer}>
-                      <svg className={styles.playButton} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                      <path d="M16.0001 2.66675C8.64008 2.66675 2.66675 8.64008 2.66675 16.0001C2.66675 23.3601 8.64008 29.3334 16.0001 29.3334C23.3601 29.3334 29.3334 23.3601 29.3334 16.0001C29.3334 8.64008 23.3601 2.66675 16.0001 2.66675ZM13.3334 22.0001V10.0001L21.3334 16.0001L13.3334 22.0001Z" fill="#7357FF"/>
-                      </svg>
+                      <img src={playcircle} />
                       <span className={styles.compositionText}>Composition 1</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                      <path fillRule="evenodd" clip-rule="evenodd" d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5ZM14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12ZM12 21C13.1046 21 14 20.1046 14 19C14 17.8954 13.1046 17 12 17C10.8954 17 10 17.8954 10 19C10 20.1046 10.8954 21 12 21Z" fill="#ADADAD"/>
-                    </svg>
+                    <img src={kebab} />
                 </div>
                 <div className={styles.composition}>
                     <div className={styles.playButtonContainer}>
-                      <svg className={styles.playButton} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                      <path d="M16.0001 2.66675C8.64008 2.66675 2.66675 8.64008 2.66675 16.0001C2.66675 23.3601 8.64008 29.3334 16.0001 29.3334C23.3601 29.3334 29.3334 23.3601 29.3334 16.0001C29.3334 8.64008 23.3601 2.66675 16.0001 2.66675ZM13.3334 22.0001V10.0001L21.3334 16.0001L13.3334 22.0001Z" fill="#7357FF"/>
-                      </svg>
+                      <img src={playcircle} />
                       <span className={styles.compositionText}>Composition 2</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                      <path fillRule="evenodd" clip-rule="evenodd" d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5ZM14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12ZM12 21C13.1046 21 14 20.1046 14 19C14 17.8954 13.1046 17 12 17C10.8954 17 10 17.8954 10 19C10 20.1046 10.8954 21 12 21Z" fill="#ADADAD"/>
-                    </svg>
+                    <img src={kebab} />
                 </div>
                 <div className={styles.composition}>
                     <div className={styles.playButtonContainer}>
-                      <svg className={styles.playButton} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                      <path d="M16.0001 2.66675C8.64008 2.66675 2.66675 8.64008 2.66675 16.0001C2.66675 23.3601 8.64008 29.3334 16.0001 29.3334C23.3601 29.3334 29.3334 23.3601 29.3334 16.0001C29.3334 8.64008 23.3601 2.66675 16.0001 2.66675ZM13.3334 22.0001V10.0001L21.3334 16.0001L13.3334 22.0001Z" fill="#7357FF"/>
-                      </svg>
+                      <img src={playcircle} />
                       <span className={styles.compositionText}>Composition 3</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                      <path fillRule="evenodd" clip-rule="evenodd" d="M14 5C14 6.10457 13.1046 7 12 7C10.8954 7 10 6.10457 10 5C10 3.89543 10.8954 3 12 3C13.1046 3 14 3.89543 14 5ZM14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12ZM12 21C13.1046 21 14 20.1046 14 19C14 17.8954 13.1046 17 12 17C10.8954 17 10 17.8954 10 19C10 20.1046 10.8954 21 12 21Z" fill="#ADADAD"/>
-                    </svg>
+                    <img src={kebab} />
                 </div>
             </div>
             <div className={styles.videoContainer}>
