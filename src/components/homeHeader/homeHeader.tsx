5e0c5c52f864018650d4c966858b8f22e028a260
Chetan Haricharan <vc.chetan.haricha@zee.com>
2023-12-01 16:31:20 +0530

    changes in home page

--- src/components/homeHeader/homeHeader.tsx
+++ src/components/homeHeader/homeHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styles from "./homeHeader.module.scss";
-import DhunIcon from "../../../public/icons/Layer_1.svg";
+import DhunIcon from "../../../public/icons/Dhun Icon.svg";
 import { MENUITEMS } from "../../utils/genAiConstant";
 
 const HomeHeader = () => {
@@ -11,7 +11,7 @@ const HomeHeader = () => {
         <div className={styles.header}>
             <div className={styles.dhunContainer}>
                 <img src={DhunIcon} />
-                <span className={styles.dhuntext}>DHUN.AI</span>
+                <span className={styles.dhuntext}>Dhun.AI</span>
             </div>
             <div className={styles.menu}>
                     {MENUITEMS.map((item,index) => (
