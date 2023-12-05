bf4dfbbb8796a0c2109b451c8f587765fd72f7a6
Chetan Haricharan <vc.chetan.haricha@zee.com>
2023-12-05 13:18:48 +0530

    files changes

--- src/components/utilConstants/apiConstants.ts
+++ src/components/utilConstants/apiConstants.ts
@@ -9,3 +9,16 @@ export enum AUTH_ENDPOINTS {
     success = "success",
     failure = "failure",
   }
+
+  export enum REGISTER_PAGE_CONSTANTS {
+    usernamePlaceholder = "Enter your name",
+    emailPlaceholder = "Enter your email",
+    passwordPlaceholder = "Enter your password",
+    usernameLabel = "Name",
+    emailLabel = "Email",
+    passwordLabel = "Password",
+    title = "Register",
+    buttonText = "Register",
+    existingUserText = "Already have an account? ",
+    loginLinkText = "Login",
+  }
