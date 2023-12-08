d9dd326c653fa6bca6122b97f9f154c2f612aa6c
Chetan Haricharan <vc.chetan.haricha@zee.com>
2023-12-08 17:49:05 +0530

    export component changes

--- src/components/resultDashboard/ResultDashboard.tsx
+++ src/components/resultDashboard/ResultDashboard.tsx
@@ -2,12 +2,14 @@
 import VideoPlayer from "../videoPlayer/VideoPlayer";
 import styles from "./ResultDashboard.module.scss";
 import ControlSelection from "../controlSelection/ControlSelection";
-import React from "react";
+import React,{useState} from "react";
 import { useGenerateStore } from "../../stores/generateStore";
 import CompositionContainer from "../compositionContainer/compositionContainer";
 import { API_STATUS_TYPES } from "../../assets/constants/apiContants";
 import LoadingSpin from "react-loading-spin";
 import compositionBackground from "../../../public/timeframeBackground.svg";
+import Emptycheckbox from "../../../public/icons/Empty checkbox.png";
+import checkbox from "../../../public/icons/Checkbox.png";
 import close from "../../../public/icons/Close.png";
 import { DndProvider, useDrag } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -16,6 +18,7 @@ import ControlPanel from "../ControlsPanel/ControlPanel";
 const ResultDashboard = () => {
   const { uploadFile, file, status }: any = useGenerateStore((state) => state);
   const timeFrames = useGenerateStore((state) => state.timeFrameData);
+  const [trackSelected,setTrackSelected] = useState<Array<string>>([]);
   // const currentTimeFrameId = useGenerateStore(
   //   (state) => state.currentTimeFrameId
   // );
@@ -26,6 +29,17 @@ const ResultDashboard = () => {
     FormD.append("file", event.target.files[0]);
     uploadFile(FormD, fileName);
   };
+
+  const handleTrackSelection = (Track: string) => {
+      if(trackSelected.includes(Track)){
+        setTrackSelected(track => {
+          let newtrk = track.filter(trk => trk!== Track);
+          return newtrk
+        } );
+      }else{
+        setTrackSelected([...trackSelected,Track]);
+      }
+  }
 
   const moveItem = (fromIndex: number, toIndex: number) => {
     // const updatedItems = [...trackItems];
@@ -65,6 +79,19 @@ const ResultDashboard = () => {
              <span className={styles.exportName}>Export</span>
              <img src={close} className={styles.closeButton} />
           </div>
+          <div onClick={() => handleTrackSelection("Composition 1") } className={styles.checkwrp}>
+              <img className={styles.chckbox} src={`${trackSelected.indexOf("Composition 1")===-1 ? `${Emptycheckbox}`: `${checkbox}`}`} />
+              <p>Composition 1</p>
+          </div>
+          <div onClick={() => handleTrackSelection("Composition 2")} className={styles.checkwrp}>
+              <img className={styles.chckbox} src={`${trackSelected.indexOf("Composition 2")===-1 ? `${Emptycheckbox}`: `${checkbox}`}`} />
+              <p>Composition 2</p>
+          </div>
+          <div onClick={() => handleTrackSelection("Composition 3")} className={styles.checkwrp}>
+              <img className={styles.chckbox} src={`${trackSelected.indexOf("Composition 3")===-1 ? `${Emptycheckbox}`: `${checkbox}`}`} />
+              <p>Composition 3</p>
+          </div>
+          <button className={styles.exportbtn} >Export Selected</button>
        </div>
       <div className={styles.timeframeContainer}>
         <div className={styles.uploadContainer}>
