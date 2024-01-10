20af19e5f238d30a17ff6cd0474d9e21e2fe4785
Sourabh Ingale <sourabh.ingale@zee.com>
2024-01-10 18:46:48 +0530

    Pause button clickable

--- src/components/videoComponent/VideoComponent.tsx
+++ src/components/videoComponent/VideoComponent.tsx
@@ -5,10 +5,12 @@ import pauseIcon from "../../../public/icons/pausegrey.svg";
 
 const VideoComponent = () => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
   const videoRef = useRef(null);
 
   const playVideoPlayer = () => {
     setIsPlaying((prev: boolean) => !prev);
+    setIsVisible((prev: boolean) => !prev);
     if (isPlaying) {
       videoRef.current?.pause();
     } else {
@@ -18,16 +20,23 @@ const VideoComponent = () => {
 
   return (
     <div className={styles.video}>
-      <video ref={videoRef} src="public/video/video.mp4" width="100%">
+      <video
+        ref={videoRef}
+        src="public/video/video.mp4"
+        width="100%"
+        onClick={playVideoPlayer}
+      >
         Sorry, your browser doesn't support embedded videos and watch it with
         your favorite video player!
       </video>
-      <img
-        src={isPlaying ? pauseIcon : playIcon}
-        alt="playIcon"
-        onClick={playVideoPlayer}
-        className={styles.playPause}
-      />
+      {isVisible && (
+        <img
+          src={isPlaying ? pauseIcon : playIcon}
+          alt="playIcon"
+          onClick={playVideoPlayer}
+          className={styles.playPause}
+        />
+      )}
     </div>
   );
 };
